782bcdbe97de56f9e17378fcf763ddd1625f5aa0
RebeccaBrasileiro <rebeccabrasileiro28@gmail>
2022-06-22 08:31:38 -0300

    web

---
+++ database/migrations/2022_06_22_071305_create_mensagem_topico_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mensagem_topicos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mensagem_id')->constraneid()
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('topico_id')->onDelete('cascade');
+            $table->unique(['mensagem_id','topico_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mensagem_topicos');
+    }
+};
