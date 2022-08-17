f202f7357eddf9f82f64e77c1d0b73ace17c5b8a
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2022-08-17 08:17:04 -0300

    laravel

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -51,8 +51,11 @@ class MensagemController extends Controller
             $mensagem->user_id = Auth::user()->id;
             $mensagem->titulo = $request->get('titulo');
             $mensagem->mensagem = $request->get('mensagem');
-            $name = $request->file('imagem')->getClientOriginalName();
-            $path = $request->file('imagem')->storeAs("public/img",$name);
+            //$name = $request->file('imagem')->getClientOriginalName();
+            //$path = $request->file('imagem')->storeAs("public/img",$name);
+            $name = $request->file('imagem')->store('','s3');
+            Storage::disk('s3')->setVisibility($name,'public');
+            $path = Storage::disk('s3')->url($name);
             $mensagem->imagem = $path;
             $mensagem->save();
             $mensagem->topicos()->attach($request->get('topico'));
@@ -101,8 +104,11 @@ class MensagemController extends Controller
         if ($validated) {
             $mensagem->titulo = $request->get('titulo');
             $mensagem->mensagem = $request->get('mensagem');
-            $name = $request->file('imagem')->getClientOriginalName();
-            $path = $request->file('imagem')->storeAs("public/img",$name);
+            //$name = $request->file('imagem')->getClientOriginalName();
+            //$path = $request->file('imagem')->storeAs("public/img",$name);
+            $name = $request->file('imagem')->store('','s3');
+            Storage::disk('s3')->setVisibility($name,'public');
+            $path = Storage::disk('s3')->url($name);
             $mensagem->imagem = $path;
             $mensagem->save();
             $mensagem->topicos()->sync($request->get('topico'));
