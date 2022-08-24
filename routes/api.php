7be28173f29a2b96aabd10a334c5e587645209e0
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2022-08-24 08:39:16 -0300

    laravel

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\MensagemController;
+use App\Http\Controllers\API\TopicoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 /*
@@ -19,4 +21,6 @@ Route::post('/login', [AuthController::class, 'login']);
 // });
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout']);
+    Route::resource("topico", TopicoController::class);
+    Route::resource("mensagem", MensagemController::class);
    });
