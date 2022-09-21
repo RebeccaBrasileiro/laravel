b2855f38435ff235ab853a2a6de7671c38709ec3
RebeccaBrasileiro <rebeccabrasileiro28@gmail.com>
2022-09-21 08:35:12 -0300

    aula21/09

---
+++ app/Http/Livewire/Push.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Push extends Component
+{
+    public function render()
+    {
+        return view('livewire.push');
+    }
+}
